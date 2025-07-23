18501c1e782ff36a96f66c3e76994119459a7412
stefanboeck <ckRENhETQoJHVJEANG7BBwuw>
2025-07-23 13:51:56 +0200

    login in .env

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Account;
-use App\Models\Category;
-use App\Models\Consent;
-use App\Models\Site;
-use App\Models\Standard;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -19,31 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Standard::create([#1
-            'standard' => 'Unbedingt erforderliche Cookies',
-            'description' => 'Diese Cookies werden benötigt, damit Sie solche grundlegenden Funktionen wie Sicherheit, Identitätsprüfung und Netzwerkmanagement nutzen können. Sie können daher nicht deaktiviert werden.',
-            'mapping' => 'necessary',
-            'checked' => 1,
-            'disabled' => 1,
-        ]);
-        Standard::create([
-            'standard' =>'Cookies für Marketingzwecke',
-            'description' =>'Cookies für Marketingzwecke werden verwendet, um die Effektivität von Werbung zu messen, Interessen von Besuchern zu erfassen und Werbeanzeigen an deren persönliche Bedürfnisse anzupassen.',
-            'mapping' => 'marketing',
-        ]);
-        Standard::create([
-            'standard' => 'Funktionale Cookies',
-            'description' => 'Funktionale Cookies werden verwendet, um bereits getätigte Angaben zu speichern und darauf basierend verbesserte und personalisierte Funktionen anzubieten.',
-            'mapping' => 'functional',
-        ]);
-        Standard::create([
-            'standard' => 'Analytics-Cookies',
-            'description' => 'Analytics-Cookies werden verwendet, um zu verstehen, wie Webseiten genutzt werden, um Fehler zu entdecken und Funktionalität von Webseiten zu verbessern.',
-            'mapping' => 'analytics',
-        ]);
         User::create([
-            'email' => 'portalix@gmail.com',
-            'password' => Hash::make('gehheim'),
+            'email' => env('TEST_EMAIL'),
+            'password' => Hash::make(env('TEST_PASSWORD')),
         ]);
     }
 }
