a4a65e471712d8c9d324e96bf249245c22d5aaec
stefanboeck <ckRENhETQoJHVJEANG7BBwuw>
2025-07-27 16:14:12 +0200

    prototype

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         if (Auth::user()->setup) {
-            return redirect('/setup');
+            //return redirect('/setup');
         }
 
         $url = $request->get('url');
