ba5079743fa01037200b1f0fe5ddf8cfe69b501f
stefanboeck <ckRENhETQoJHVJEANG7BBwuw>
2025-07-30 18:34:40 +0200

    progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,6 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li-->
                             @endif
-x
                             @if (Route::has('register'))
                                 <!--li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
